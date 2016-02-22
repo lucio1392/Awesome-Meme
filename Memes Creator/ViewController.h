5f0fe90295090ca23c7975d1b3bb7895161b86a4
sunsunai <theptv13@gmail.com>
2016-02-22 21:29:14 +0700

    Initial Commit

---
+++ Memes Creator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memes Creator
+//
+//  Created by Vinh The on 2/22/16.
+//  Copyright © 2016 Vinh The. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Memes Creator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Memes Creator
+//
+//  Created by Vinh The on 2/22/16.
+//  Copyright © 2016 Vinh The. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
